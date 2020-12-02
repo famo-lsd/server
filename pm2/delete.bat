fa3745a84bdd4b03b0395c50116763038c98075b
António Garcez <antonio.garcez@famo.pt>
2020-12-02 17:27:40 +0000

    [DEV] Alteração do ficheiro de configurações do pm2 para que suporte o inicio de duas instancias de Node.js.

--- pm2/delete.bat
+++ pm2/delete.bat
@@ -1,1 +1,1 @@
-pm2 delete 0
+pm2 delete all
--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -2,12 +2,26 @@ module.exports = {
   apps: [{
     name: 'Server - Node.js',
     cwd: 'C:/FAMO_NODE/Server/dist/',
-    // cwd: 'L:/Git/PDA/pda/server/dist/',
     script: 'app.js',
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '512M',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'Database - Redis',
+    cwd: 'C:/FAMO_NODE/Server/dist/',
+    script: 'clean.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '256M',
     env: {
       NODE_ENV: 'development'
     },
